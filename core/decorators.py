28255620ff132dd5b58dc49fefc65946e2b71015
subham8828 <jaishreeram4573@gmail.com>
2026-05-24 14:58:29 +0530

    fixed join error

--- core/decorators.py
+++ core/decorators.py
@@ -103,10 +103,6 @@ def handle_error(func: Callable) -> Callable:
         except BaseException:
             lang = config.LANGUAGE
         try:
-           # await app.join_chat("AsmSafone")
-        except UserAlreadyParticipant:
-            pass
-        try:
             return await func(client, obj, *args)
         except Exception:
             id = int(time.time())
